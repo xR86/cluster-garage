c65974dd09236e76edd54ec4bb94e8a81f8ea048
Dan Alexandru <dan.alexandru@info.uaic.ro>
2016-06-22 09:12:04 +0300

    added nodemon and mongoose find

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-var server = require('../app');
+//var server = require('../app');
 var mongoose = require('mongoose');
 
 var helloWorld = 'Welcome, MEAN stack !';
+
+function find (collec, query, callback) {
+    mongoose.connection.db.collection(collec, function (err, collection) {
+		collection.find(query).toArray(callback);
+    });
+}
+
+find('test', {"_id": { $eq: "5769621cb583fb11980b0213" }}, function (err, data) {
+    console.log(data);
+    helloWorld = data;
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
